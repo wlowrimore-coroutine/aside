189748a8aeffa8b3bca218c4c92886aa371053ad
William Lowrimore <wlowrimore@gmail.com>
2022-09-24 11:53:42 -0500

    setting up the chat broadcasting

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :rooms
   root 'pages#home'
-  get 'user/:id', to: 'users#show', as: 'user'
+  devise_for :users
   devise_scope :user do
     #Redirects users signing out back to sign-in
     get "users", to: "devise/sessions#new"
   end
-  devise_for :users
+  get 'user/:id', to: 'users#show', as: 'user'
 end
